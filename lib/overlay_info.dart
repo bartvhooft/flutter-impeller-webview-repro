b3f8798d6158071e1b04290bf8236a6b03ece2d9
Bart van Hooft <vanhooftbart@gmail.com>
2026-06-03 11:48:01 +0200

    feat: add InfoOverlay widget

---
+++ lib/overlay_info.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class InfoOverlay extends StatelessWidget {
+  const InfoOverlay({
+    super.key,
+    required this.flutterVersion,
+    required this.impellerEnabled,
+    required this.deviceModel,
+  });
+
+  final String flutterVersion;
+  final bool impellerEnabled;
+  final String deviceModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: MediaQuery.of(context).padding.top + 8,
+      left: 8,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+        decoration: BoxDecoration(
+          color: Colors.black.withValues(alpha: 0.6),
+          borderRadius: BorderRadius.circular(6),
+        ),
+        child: DefaultTextStyle(
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            fontFamily: 'monospace',
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Flutter: $flutterVersion'),
+              Text(
+                'Impeller: ${impellerEnabled ? "enabled" : "disabled"}',
+                style: TextStyle(
+                  color:
+                      impellerEnabled ? Colors.redAccent : Colors.greenAccent,
+                ),
+              ),
+              Text('Device: $deviceModel'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
